86a3b9890bf9cacec2a1db66bee437aae2892f8d
pollachidotcom <pollachidotcom@gmail.com>
2025-02-13 17:06:07 +0530

    NewOlauncher
    added icons

--- app/src/main/java/app/olauncher/ui/AppDrawerAdapter.kt
+++ app/src/main/java/app/olauncher/ui/AppDrawerAdapter.kt
@@ -158,7 +158,11 @@ class AppDrawerAdapter(
                 appHideLayout.visibility = View.GONE
                 renameLayout.visibility = View.GONE
                 appTitle.visibility = View.VISIBLE
+               val packageManager = root.context.packageManager
+                val appIcon = packageManager.getApplicationIcon(appModel.appPackage)
+                appIconView.setImageDrawable(appIcon)
                 appTitle.text = appModel.appLabel + if (appModel.isNew == true) " ✦" else ""
+
                 appTitle.gravity = appLabelGravity
                 otherProfileIndicator.isVisible = appModel.user != myUserHandle
 
